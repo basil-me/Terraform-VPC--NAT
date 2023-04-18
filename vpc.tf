7683edfd7ced809e9790f84e15b5d66c14f2a065
basil <basil68@ymail.com>
2023-04-18 11:42:23 +0530

    files

---
+++ vpc.tf
@@ -0,0 +1,189 @@
+provider "aws" {
+  region = var.region
+}
+
+# VPC
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name                                           = "${var.project}-vpc",
+    "kubernetes.io/cluster/${var.project}-cluster" = "shared"
+  }
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count = var.availability_zones_count
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                                           = "${var.project}-public-sg"
+    "kubernetes.io/cluster/${var.project}-cluster" = "shared"
+    "kubernetes.io/role/elb"                       = 1
+  }
+
+  map_public_ip_on_launch = true
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count = var.availability_zones_count
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, count.index + var.availability_zones_count)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                                           = "${var.project}-private-sg"
+    "kubernetes.io/cluster/${var.project}-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"              = 1
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    "Name" = "${var.project}-igw"
+  }
+
+  depends_on = [aws_vpc.this]
+}
+
+# Route Table(s)
+# Route the public subnet traffic through the IGW
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project}-Public-RT"
+  }
+}
+
+
+# Route table and subnet associations
+resource "aws_route_table_association" "internet_access" {
+  count = var.availability_zones_count
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "internet_access2" {
+  count = var.availability_zones_count
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.main-two.id
+}
+# NAT Elastic IP
+resource "aws_eip" "main" {
+  vpc = true
+
+  tags = {
+    Name = "${var.project}-ngw-ip"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.main.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project}-ngw"
+  }
+}
+
+# NAT Elastic IP
+resource "aws_eip" "main-two" {
+  vpc = true
+
+  tags = {
+    Name = "${var.project}-ngw-ip-two"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "main-two" {
+  allocation_id = aws_eip.main-two.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project}-ngw-two"
+  }
+}
+
+# Add route to route table
+resource "aws_route" "main" {
+  route_table_id         = aws_vpc.this.default_route_table_id
+  nat_gateway_id         = aws_nat_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  
+}
+
+# Add route to route table
+resource "aws_route_table" "main-two" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main-two.id
+  }
+
+  tags = {
+    Name = "${var.project}-Private-RT"
+  }
+}
+
+
+# Security group for public subnet
+resource "aws_security_group" "public_sg" {
+  name   =  "${var.project}-Public-sg"
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project}-Public-sg"
+  }
+}
+
+# Security group traffic rules
+resource "aws_security_group_rule" "sg_ingress_public_443" {
+  security_group_id = aws_security_group.public_sg.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sg_ingress_public_80" {
+  security_group_id = aws_security_group.public_sg.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sg_egress_public" {
+  security_group_id = aws_security_group.public_sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
